db6886188335aeba43ca3143df70ad88c21891a5
Dominik Łapiński <dominiklapinski.pl@gmail.com>
2022-03-29 21:57:27 +0200

    Adds s3 bucket

--- pulumi-sina/__main__.py
+++ pulumi-sina/__main__.py
@@ -1,3 +1,4 @@
+import json
 import pulumi
 import pulumi_aws as aws
 
@@ -32,9 +33,34 @@ http_listener = aws.lb.Listener('sina-lb-http-listener',
 asg_attach = aws.autoscaling.Attachment('sina-asg-attachment',
     autoscaling_group_name=asg.id,
     alb_target_group_arn=tg.arn,
-    opts=pulumi.ResourceOptions(depends_on=[tg, asg]),
+)
+
+bucket = aws.s3.Bucket('sina-bucket',
+    bucket='pl-sina-bucket',
+    tags={
+        'Name': 'Sina Bucket',
+    })
+
+def public_read_policy_for_bucket(arn):
+    return json.dumps({
+        'Version': '2012-10-17',
+        'Statement': [
+            {
+                'Sid': 'PublicReadPolicy',
+                'Effect': 'Allow',
+                'Principal': '*',
+                'Action': [
+                    's3:GetObject',
+                ],
+                'Resource': f'{arn}/*',
+            }
+        ],
+    })
+
+bucket_policy = aws.s3.BucketPolicy('sina-public-read-bucket-policy',
+    bucket=bucket.id,
+    policy=bucket.arn.apply(public_read_policy_for_bucket),
 )
 
 pulumi.export('Application Load Balancer DNS name', lb.dns_name)
-# pulumi.export('AWS Instance public IP', web.public_ip)
-# pulumi.export('AWS Instance public dns', web.public_dns)
+pulumi.export('S3 Bucket URL', bucket.bucket_domain_name)
